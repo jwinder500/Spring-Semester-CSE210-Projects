bdb3bfa0e43a93993c624b1e0a344214533bccf8
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-04-22 21:20:14 -0600

    Did projects due today
    1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "0";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (number >= 70)
+        {
+            Console.WriteLine($"Congragulations you passed, your letter grade is: {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"You failed, your letter grade is: {letter}");
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
