e062aceb1a77b63bfef2ea1b57264a314caf1dcb
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-04-26 11:16:31 -0600

    Wrote program
    yes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "Y";
+        while (response == "Y")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,101);
+            string correct = "Wrong";
+            while (correct == "Wrong")
+            {
+                Console.WriteLine("What is the magic number");
+                int guess = int.Parse(Console.ReadLine());
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    correct = "Right";
+                }
+            }
+            Console.WriteLine("Do you want to continue? (Y/N)");
+            response = Console.ReadLine();
+        }
+    Console.WriteLine("Have A Great Day!");
     }
 }
