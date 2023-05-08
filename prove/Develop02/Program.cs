2de32f6f7d3080e25d3c4701a7cd99d9db100143
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-05-08 11:13:11 -0600

    changed journal class
    changed journal project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,22 +1,36 @@
+using System.Collections.Generic;
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Develop02{
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
-    }
-}
+        static void Main(string[] args)
+        {
+            //add some test entries
+            Entry myEntry = new Entry();
+            myEntry.StorePrompt("What is your name?");
+            myEntry.StoreResponse("James Winder");
+            myEntry.StoreDate("8 May 2023");
 
-class Prompts
-{
+            //add some test entries
+            Entry yourEntry = new Entry();
+            yourEntry.StorePrompt("What is your favorite food?");
+            yourEntry.StoreResponse("Penut Butter");
+            yourEntry.StoreDate("8 May 2023");
 
-}
-class Entry
-{
-    
-}
-class Journal
-{
+            //Create journal to test
+            Journal journal = new Journal();
+            journal.StoreEntry(myEntry);
+            journal.StoreEntry(yourEntry);
 
+            //Show the journal contents
+            List<Entry> entries = journal.GetEntries();
+            foreach (Entry entry in entries)
+            {
+                string message
+                    = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}";
+                Console.WriteLine(message);
+            }
+        }
+    }  
 }
