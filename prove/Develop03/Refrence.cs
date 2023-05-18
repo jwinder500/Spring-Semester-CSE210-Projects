9942cd552edf1f2345a8fe2e45fdc1d4923ce652
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-05-18 11:02:30 -0600

    finished some classes
    word and refrence

--- prove/Develop03/Refrence.cs
+++ prove/Develop03/Refrence.cs
@@ -7,10 +7,17 @@ namespace Develop03
     {
         //This class holds the refrence
         private string refrence;
+        private List<string> listRefrence = new List<string>();
 
         public string getRefrence()
         {
-            Word NewRefrence = new Word();
+            Word newRefrence = new Word();
+            listRefrence = newRefrence.SplitWords();
+            while (listRefrence.Count() > 2)
+            {
+                listRefrence.RemoveAt(listRefrence.Count());
+            }
+            refrence = string.Join(" ", listRefrence);
             return refrence;
         }
     }
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -8,6 +8,38 @@ namespace Develop03
         // This class breaks apart words into scritures, and then hides some of them
         // It also increases the amout of words hidden each time
         private List<string> words = new List<string>{};
-         
+        private List<string> hiddenWords = new List<string>{};
+        private int numberOfWords;
+        Random random = new Random();
+        public List<string> SplitWords()
+        {
+            Scripture newScripture = new Scripture();
+            string scripture = newScripture.selectRandomScripture();
+            List<string> words = scripture.Split(" ").ToList();
+            return words;
+        }
+        public List<string> RemoveReference()
+        {
+            words.RemoveAt(0);
+            words.RemoveAt(0);
+            return words;
+        }
+        private int Count()
+        {
+            numberOfWords = words.Count();
+            return numberOfWords;
+        }
+        public List<string> HideWords()
+        {
+            hiddenWords = words;
+            int number = hiddenWords.Count();
+            hiddenWords[number] = "_";
+            return hiddenWords;
+        }
+        public string Display()
+        {
+            string adjustedWords = string.Join(" ", hiddenWords);
+            return adjustedWords;
+        }
     }
 }
