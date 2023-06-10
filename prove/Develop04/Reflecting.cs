88a0402f3be3abedb62dd37821f06daf651457c0
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-06-10 13:40:34 -0600

    updated something
    .

--- prove/Develop04/Reflecting.cs
+++ prove/Develop04/Reflecting.cs
@@ -21,7 +21,6 @@ namespace Develop04
 
             getReady();
 
-
             while(time >= 0)
             {
                 
@@ -35,24 +34,23 @@ namespace Develop04
 
         private List<string> prompt = new List<string>
         {
-            "",
-            "",
-            "",
-            ""
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
         };
 
         private List<string> questions = new List<string>
         {
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "Why was this expiriance meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this expirience?",
+            "What could you learn from this expirience that applies to other situations?",
+            "What did you learn about yourself through this expirience?",
+            "How can you keep this expirience in mind in the future?"
         };
-
     }
 }
