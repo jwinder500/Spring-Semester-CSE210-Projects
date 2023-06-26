60e5ec294485e1d1e4b785d6c3176834403bc1cf
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-06-26 10:41:13 -0600

    Created every Class
    let there be classes

---
+++ prove/Develop05/Eternal_Goal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Develop05
+{
+    public class Eternal_Goal:Goal
+    {
+        public override void CreateGoal()
+        {
+
+        }
+    }
+}
---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Develop05
+{
+    public class Goal
+    {
+        protected virtual void CreateGoal()
+        {
+            
+        }
+    }
+}
