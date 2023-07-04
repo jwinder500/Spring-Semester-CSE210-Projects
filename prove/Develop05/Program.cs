c954a8177f92313edf1b42295c8fd50fb5554eac
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-07-04 14:58:01 -0600

    fixed spacing
    s

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,49 +1,48 @@
 using System;
 
-namespace Develop05
-{
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int Keep_Going = 1;
+        while (Keep_Going != 0 )
         {
-            int Keep_Going = 1;
-            while (Keep_Going != 0 )
+            public Goal goal = new Goal();
+            public  Points points = new Points();
+                        
+            //menu
+
+            Console.WriteLine("What would you like to do today? ");
+            Console.WriteLine("0: Quit. ");
+            Console.WriteLine("1: Create a Goal. ");
+            Console.WriteLine("2: View all Goals. ");
+            Console.WriteLine("3: View your level. ");
+            Console.WriteLine("4: View Points. ");
+
+            //selection
+            string selection = Console.ReadLine();
+            Keep_Going = int.Parse(selection);
+
+            //what to do based on selection
+            if (Keep_Going == 0)
             {
-                public Goal goal = new Goal();
-                public  Points points = new Points();
-                //menu
-                Console.WriteLine("What would you like to do today? ");
-                Console.WriteLine("0: Quit. ");
-                Console.WriteLine("1: Create a Goal. ");
-                Console.WriteLine("2: View all Goals. ");
-                Console.WriteLine("3: View your level. ");
-                Console.WriteLine("4: View Points. ");
 
-                //selection
-                string selection = Console.ReadLine();
-                Keep_Going = int.Parse(selection);
+            }
+            else if (Keep_Going == 0)
+            {
 
-                //what to do based on selection
-                if (Keep_Going == 0)
-                {
+            }
+            else if (Keep_Going == 0)
+            {
 
-                }
-                else if (Keep_Going == 0)
-                {
+            }
+            else if (Keep_Going == 0)
+            {
 
-                }
-                else if (Keep_Going == 0)
-                {
+            }
+            else
+            {
 
-                }
-                else if (Keep_Going == 0)
-                {
-
-                }
-                else
-                {
-
-                }
             }
         }
     }
