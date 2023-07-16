fc75f0eb6486fbe25b7c28bebcdb2913895ceec1
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2023-07-15 21:25:11 -0600

    update
    yupdate

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -4,6 +4,14 @@ namespace Foundation3
 {
     public class Event
     {
-
+        protected string title;
+        protected string description;
+        protected string date(int day, int month, int year)
+        {
+            protected string MMDDYY = $"{month}/{day}/{year}";
+            return MMDDYY;
+        }
+        protected int time;
+        protected string address;
     }
 }
